91870e4763116276cc9c58a2bb3a991193aaaa4a
souhaib <souhaib.smsm50@gmail.com>
2025-01-12 16:26:36 +0100

    CPP04

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,9 @@ ScalarConverter::~ScalarConverter() {}
 
 ScalarConverter::ScalarConverter(const ScalarConverter &other) {}
 
-ScalarConverter& ScalarConverter::operator=(const ScalarConverter &other) {}
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter &other) {
+    return *this;
+}
 
 
 enum e_type {
@@ -18,10 +20,13 @@ enum e_type {
 }; 
 
 bool    isFloat(const std::string& s) {
-    if (s[s.size()] != 'f') return false;
+    if (s[s.length() - 1] != 'f') return false;
+    
+
     int start = 0;
     if (s[0] == '-' || s[0] == '+') start = 1;
-    return s.find_first_not_of("0123456789", start) == std::string::npos;
+    std::string check = s.substr(0, s.size() - 1);
+    return check.find_first_not_of("0123456789.", start) == std::string::npos;
 }
 
 bool    isInt(const std::string& s){
@@ -31,39 +36,42 @@ bool    isInt(const std::string& s){
 }
 
 bool    isDouble(const std::string& s){
-    
+    return true;
 }
 
 e_type getType(const std::string& s) {
     if (s.size() == 1 && isalpha(s[0])) return CHAR;
-    // if (isFloat(s)) return FLOAT;
     if (isInt(s)) return INT;
+    if (isFloat(s)) return FLOAT;
     // if (isDouble(s)) return DOUBLE;
     return UNKOWN;
 }
 
+
 void    ScalarConverter::convert(const std::string& literal) {
-    // parse input
-    /*
-        char
-        int
-        float
-        double
-    */
-    // for (int i=0; i< literal.size(); i++) {
-    //     if (!isprint(literal[i])) throw std::runtime_error("non printable detetcted");
-    // }
+
+    int dotCount = 0;
+    if (literal.empty()) {
+        throw std::runtime_error("invalid input");
+    }
+    for (int i=0; i< literal.size(); i++) {
+        if (literal[i] == '.') dotCount++;
+        if (dotCount > 1 || !isprint(literal[i])) throw std::runtime_error("invalid input");
+    }
 
     e_type type = getType(literal);
+    std::cout << type << "\n";
     if (type == INT) {
-        int val = std::strtol(literal.c_str(), 0, 10);
-        std::cerr << val << "\n";
-        if (errno == ERANGE) throw std::runtime_error("overflow detected");
+        long val = std::strtol(literal.c_str(), 0, 10);
+
+        if (val > INT_MAX || val < INT_MIN) {
+            throw std::runtime_error("overflow detected");
+        }
         if (val > 255 || val < 0 || !isprint(val))
             std::cout << "char: Non displayable" << std::endl;
         else
             std::cout << "char: " << static_cast<char>(val) << std::endl;
-        std::cout << "int: " << val << std::endl;
+        std::cout << "int: " << static_cast<int>(val) << std::endl;
         std::cout << "float: " << static_cast<float>(val) <<  std::endl; 
         std::cout << "double: " << static_cast<double>(val) << std::endl;
     }
@@ -71,12 +79,33 @@ void    ScalarConverter::convert(const std::string& literal) {
         char val = literal[0];
         std::cout << "char: " << val << std::endl;
         std::cout << "int: " << static_cast<int>(val) << std::endl;
-        std::cout << "float: " << static_cast<float>(val) <<  std::endl; 
-        std::cout << "double: " << static_cast<double>(val) << std::endl;
+        std::cout << "float: " << static_cast<float>(val) << ".0f" << std::endl; 
+        std::cout << "double: " << static_cast<double>(val) << ".0" << std::endl;
     }
     else if (type == FLOAT) {
+        float val = strtof(literal.c_str(), 0);
+        bool zeroFract = literal.find(".0f") != std::string::npos;
+        if (val > INT_MAX || val < INT_MIN) {
+            throw std::runtime_error("overflow detected");
+        }
+        if (val > 255 || val < 0 || !isprint(val))
+            std::cout << "char: Non displayable" << std::endl;
+        else
+            std::cout << "char: " << static_cast<char>(val) << std::endl;
+        std::cout << "int: " << static_cast<int>(val) << std::endl;
+        if (zeroFract) {
+            std::cout << "float: " << static_cast<float>(val) << ".0f" <<  std::endl; 
+            std::cout << "double: " << static_cast<double>(val) << ".0" << std::endl;
+        }
+        else {
+            std::cout << "float: " << literal <<  std::endl; 
+            std::cout << "double: " << static_cast<double>(val) << std::endl;
+        }
+    }
+    else if (type == DOUBLE) {
 
     }
-    if (type == UNKOWN) throw std::runtime_error("UNKOWN type");
-
+    else {
+        throw std::runtime_error("UNKOWN type");
+    }
 }
